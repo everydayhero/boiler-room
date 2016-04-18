9b52d62b21a1778cfcd18cded79661685aa68572
Luke Brooker <mail@lukebrooker.com>
2016-04-18 22:44:13 +1000

    Facebook metatags

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -7,6 +7,10 @@ const Document = ({ title, description, content }) => (
       <title>{title}</title>
       <meta name='description' content={description} />
       <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+      <meta property='og:url' content='http://ifgirlsrantheworld.com' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content='/layouts/Hero/running-girl.jpg' />
       <link rel='icon' type='image/x-icon' href='/layouts/Document/favicon.ico' />
       <link rel='apple-touch-icon' href='/layouts/Document/favicon.png' />
       <script src='https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js' />
