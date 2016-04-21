44b081bf2c234d0f119c7e6e622a02236c47c485
Michelle Jones <michelle.jones@everydayhero.com>
2016-04-21 15:51:49 +1000

    Remove share text box

--- source/layouts/BringYourBff/index.js
+++ source/layouts/BringYourBff/index.js
@@ -1,56 +1,33 @@
 import React, { Component, PropTypes } from 'react'
-import Textarea from 'react-textarea-autosize'
 
 import styles from './styles.css'
 
-class BringYourBff extends Component {
-  constructor (props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.state = {
-      shareMessage: props.placeholder
-    }
+const BringYourBff = ({
+  title,
+  subtitle,
+  placeholder
+}) => {
+  const shareTitle = 'If Girls Ran The World'
+  const shareUrl = 'http://ifgirlsrantheworld.com'
+  const emailBody = `${placeholder} Check out ${shareUrl}`
+  const shareLinks = {
+    email: `mailto:?subject=${shareTitle}&body=${emailBody}`,
+    facebook: `http://www.facebook.com/sharer.php?u=${shareUrl}`,
+    twitter: `https://twitter.com/intent/tweet?text=${placeholder}&url=${shareUrl}`
   }
-  handleChange (e) {
-    this.setState({
-      shareMessage: e.target.value
-    })
-  }
-  render () {
-    const {
-      title,
-      subtitle
-    } = this.props
-    const {
-      shareMessage
-    } = this.state
-    const shareTitle = 'If Girls Ran The World'
-    const shareUrl = 'http://ifgirlsrantheworld.com'
-    const emailBody = `${shareMessage} Check out ${shareUrl}`
-    const shareLinks = {
-      email: `mailto:?subject=${shareTitle}&body=${emailBody}`,
-      facebook: `http://www.facebook.com/sharer.php?u=${shareUrl}`,
-      twitter: `https://twitter.com/intent/tweet?text=${shareMessage}&url=${shareUrl}`
-    }
-    return (
-      <div className={styles.root}>
-        <h2 className={styles.title}>{title}</h2>
-        <h3 className={styles.subtitle}>{subtitle}</h3>
-        <div className={styles.share}>
-          <Textarea
-            className={styles.input}
-            maxRows={7}
-            onChange={this.handleChange}
-            value={shareMessage} />
-          <div className={styles.buttons}>
-            <a href={shareLinks.email} className={styles.button}>Email</a>
-            <a href={shareLinks.twitter} target='_blank' className={styles.button}>Twitter</a>
-            <a href={shareLinks.facebook} target='_blank' className={styles.button}>Facebook</a>
-          </div>
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>{title}</h2>
+      <h3 className={styles.subtitle}>{subtitle}</h3>
+      <div className={styles.share}>
+        <div className={styles.buttons}>
+          <a href={shareLinks.email} className={styles.button}>Email</a>
+          <a href={shareLinks.twitter} target='_blank' className={styles.button}>Twitter</a>
+          <a href={shareLinks.facebook} target='_blank' className={styles.button}>Facebook</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 BringYourBff.propTypes = {
