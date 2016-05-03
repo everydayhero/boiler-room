c2c327a38aff6e6f69ed2626f0c80ea36023600a
Michelle Jones <michelle.jones@everydayhero.com>
2016-05-03 10:41:50 +1000

    Remove duplicate meta tag

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -9,7 +9,6 @@ const Document = ({ title, description, content }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>{title}</title>
       <meta name='description' content={description} />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
       <meta property='og:url' content='http://ifgirlsrantheworld.com' />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
