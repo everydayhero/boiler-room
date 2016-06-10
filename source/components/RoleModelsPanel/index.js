01c275e6fffb0ba001150c4a4df813acb51ea4fc
Luke Brooker <mail@lukebrooker.com>
2016-06-10 23:17:51 +1000

    Tweak to role models style

--- source/components/RoleModelsPanel/index.js
+++ source/components/RoleModelsPanel/index.js
@@ -11,7 +11,7 @@ export default ({
   ...props
 }) => (
   <div {...props} className={styles.root}>
-      <img className={styles.image} src={image} alt={name}/>
+    <img className={styles.image} src={image} alt={name}/>
     <h3 className={styles.title}>{name}</h3>
       <p className={styles.subHeading}>{subheading}</p>
       <p className={styles.content}>{content}</p>
--- source/layouts/RoleModels/index.js
+++ source/layouts/RoleModels/index.js
@@ -19,7 +19,7 @@ export default ({
           <div
             key={key}
             className={styles.item}>
-            <RoleModelsPanel invert={(key % 2) !== 0} {...panel}/>
+            <RoleModelsPanel {...panel}/>
           </div>
         ))}
       </div>
