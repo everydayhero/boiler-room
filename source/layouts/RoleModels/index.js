cc00bc8c1e8111926a3605181b18f98278a7e6b6
Michelle Jones <micki_j@live.com.au>
2016-06-10 15:48:49 +1000

    Update role models section

--- source/layouts/RoleModels/index.js
+++ source/layouts/RoleModels/index.js
@@ -10,8 +10,8 @@ export default ({
   <div className={styles.root}>
     <div className={styles.fancyBox}>
       <h2 className={styles.title}>ROLE MODELS</h2>
-      <p className={styles.tagline}>They inspire us, and hope they’ll inspire you too.</p>
       <p className={styles.intro}>Our Role Models are a passionate and eclectic group of women from across the country. They are strong, bold, beautiful, and committed to empowering girls around the globe through their involvement with If Girls Ran the World.   We admire their passion, their purpose, and their positivity.</p>
+      <p className={styles.tagline}>They inspire us, we hope they’ll inspire you too.</p>
     </div>
     <div className={styles.content}>
       <div className={styles.panels}>
