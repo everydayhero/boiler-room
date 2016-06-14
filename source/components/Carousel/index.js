ed98957ed370b43dffdae6cc230e572342f6436a
Luke Brooker <mail@lukebrooker.com>
2016-06-14 19:45:41 +1000

    Basic carousel component

---
+++ source/components/Carousel/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+import styles from './styles.css'
+
+export default class extends Component {
+  constructor () {
+    super()
+    this.handlePrevious = this.handlePrevious.bind(this)
+    this.handleNext = this.handleNext.bind(this)
+  }
+  handlePrevious () {
+    this._scroller.scrollLeft = this._scroller.scrollLeft - (this._scroller.scrollWidth / this.props.items.length)
+  }
+  handleNext () {
+    this._scroller.scrollLeft = this._scroller.scrollLeft + (this._scroller.scrollWidth / this.props.items.length)
+  }
+  render () {
+    const {
+      items,
+      widthUnit = 'rem',
+      itemWidth = '16',
+      ItemComponent,
+      ...props
+    } = this.props
+    return (
+      <div {...props}
+        className={styles.root}>
+        <button className={styles.buttonPrevious}
+          onClick={this.handlePrevious}
+          title='Previous'>
+          <svg className={styles.icon} viewBox='0 0 32 32'>
+            <polyline points='20.3,5.4 9.7,16 20.1,26.5'/>
+          </svg>
+        </button>
+        <button className={styles.buttonNext}
+          onClick={this.handleNext}
+          title='Next'>
+          <svg className={styles.icon} viewBox='0 0 32 32'>
+            <polyline points='10.9,5.3 21.3,16.1 10.7,26.4'/>
+          </svg>
+        </button>
+        <div className={styles.scroller}
+          style={{
+            scrollSnapPointsX: `repeat(${itemWidth + widthUnit})`,
+            WebkitScrollSnapPointsX: `repeat(${itemWidth + widthUnit})`,
+            msScrollSnapPointsX: `repeat(${itemWidth + widthUnit})`
+          }}
+          ref={(c) => this._scroller = c}>
+          <div className={styles.wrapper}
+            style={{
+              width: items.length * itemWidth + widthUnit
+            }} >
+            {items.map((item, i) => (
+              <ItemComponent {...item} style={{width: itemWidth + widthUnit}} key={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- source/layouts/RoleModels/index.js
+++ source/layouts/RoleModels/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Carousel from '../../components/Carousel'
 import RoleModelsPanel from '../../components/RoleModelsPanel'
 import styles from './styles.css'
 
@@ -8,21 +9,15 @@ export default ({
   panels
 }) => (
   <div className={styles.root}>
-    <div className={styles.fancyBox}>
-      <h2 className={styles.title}>ROLE MODELS</h2>
-      <p className={styles.intro}>Our Role Models are a passionate and eclectic group of women from across the country. They are strong, bold, beautiful, and committed to empowering girls around the globe through their involvement with If Girls Ran the World.   We admire their passion, their purpose, and their positivity.</p>
-      <p className={styles.tagline}>They inspire us, we hope they’ll inspire you too.</p>
+    <div className={styles.header}>
+      <h2 className={styles.title}>Role Models</h2>
+      <div className={styles.lead}>
+        <p className={styles.intro}>Our Role Models are a passionate and eclectic group of women from across the country. They are strong, bold, beautiful, and committed to empowering girls around the globe through their involvement with If Girls Ran the World.   We admire their passion, their purpose, and their positivity.</p>
+        <p className={styles.tagline}>They inspire us, we hope they’ll inspire you too.</p>
+      </div>
     </div>
     <div className={styles.content}>
-      <div className={styles.panels}>
-        {panels.map((panel, key) => (
-          <div
-            key={key}
-            className={styles.item}>
-            <RoleModelsPanel {...panel}/>
-          </div>
-        ))}
-      </div>
+      <Carousel items={panels} ItemComponent={RoleModelsPanel} />
     </div>
   </div>
 )
