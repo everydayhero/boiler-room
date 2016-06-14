76b03010da84f6aa48dd554c838b7fd41406397c
Michelle Jones <micki_j@live.com.au>
2016-06-14 16:25:36 +1000

    Styling fix ups

--- source/content/about.md
+++ source/content/about.md
@@ -6,7 +6,7 @@ Studies show that when women are supported and empowered, all of society benefit
 
 So, this October, join us as we collectively and virtually run around the world raising funds and awareness to empower girls globally.  Whether you run once or everyday, raise $10 or $10,000, your time, effort, energy and voice will change lives and build communities all over the world.
 
-Run wherever and whenever and track your miles [Strava](https://strava.com) or [Map My Fitness](http://www.mapmyfitness.com/) during the month long virtual challenge to add to our collective progress.  Share your story with friends and family to raise funds for the cause that means the most to you.
+Run (or walk) wherever and whenever and track your miles [Strava](https://strava.com) or [Map My Fitness](http://www.mapmyfitness.com/) during the month long virtual challenge to add to our collective progress. Share your story with friends and family to raise funds for the cause that means the most to you.
 
-**Although worlds apart, we’re all in this together.  Virtually, anything is possible.**
+**Although worlds apart, we’re all in this together. Virtually, anything is possible.**
 
--- source/layouts/Register/index.js
+++ source/layouts/Register/index.js
@@ -8,7 +8,7 @@ export default ({
 }) => (
   <div className={styles.root} id='register'>
     <RegisterTitle />
-    <p className={styles.intro}>Choose from one of our charity partners below and commit to a distance and fundraising goal.  Then invite your friends to join you and get ready to run the world.</p>
+    <p className={styles.intro}>Choose from one of our charity partners below and set a distance and fundraising goal. Then invite your friends to join you and get ready to run the world.</p>
     <ul className={styles.primaryList}>
       {charities.primary.map((charity, i) => (
         <li className={styles.primaryItem} key={i}>
@@ -20,7 +20,7 @@ export default ({
     </ul>
     <div className={styles.footerText}>
       <h3 className={styles.footerTitle}>Don't see the cause that’s closest to your heart?</h3>
-      <p><a href='#'>Find the cause</a> that means the most to you. Still can’t find your cause?  Email us at <a href='mailto:hello@everydayhero.com'>hello@everydayhero.com</a></p>
+      <p><a href='https://ifgirlsrantheworld.eventbrite.com'>Register here</a> and then search for the cause that means the most to you. Still can’t find your cause?  Email us at <a href='mailto:hello@everydayhero.com'>hello@everydayhero.com</a></p>
     </div>
   </div>
 )
