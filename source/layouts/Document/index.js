bfeb03cc56fffbce0cc2e44d4231c026ddcceb10
Michelle Jones <micki_j@live.com.au>
2016-06-29 10:53:14 +1000

    Update google analytics and tag manager

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -34,6 +34,25 @@ const Document = ({ title, description, content }) => (
       <script src='https://cdnjs.cloudflare.com/ajax/libs/dot/1.0.3/doT.js' />
       <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.min.js' />
       <script src='/main.js' />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+            ga('create', 'UA-77699721-1', 'auto');
+            ga('send', 'pageview');`
+        }}
+        />
+      <script async src='https://www.google-analytics.com/analytics.js'></script>
+
+      <noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-PN6K34' height='0' width='0' style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PN6K34');`
+        }}
+      />
     </body>
   </html>
 )
