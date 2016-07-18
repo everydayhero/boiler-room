d27baae0bca0e9936b9d39e3ae2126a5729e5dd2
Luke Brooker <mail@lukebrooker.com>
2016-07-19 07:56:34 +1000

    Remove extraneous external files

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -21,10 +21,6 @@ const Document = ({
       <link rel='icon' type='image/x-icon' href='/layouts/Document/favicon.ico' />
       <link rel='apple-touch-icon' href='/layouts/Document/favicon.png' />
       <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' />
-      <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.3.15/slick.css' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-shim.min.js' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-sham.min.js' />
       <link rel='stylesheet' href='/vendor.css' />
       <link rel='stylesheet' href='/main.css' />
     </head>
