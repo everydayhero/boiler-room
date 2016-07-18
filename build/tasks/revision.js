c3084b16aceb2caf8a08f918395546512d542423
Luke Brooker <mail@lukebrooker.com>
2016-07-19 08:08:43 +1000

    Fix build linting

--- build/tasks/revision.js
+++ build/tasks/revision.js
@@ -3,10 +3,8 @@ const rev = require('gulp-rev')
 const path = require('path')
 
 const config = require('./config')
-const SOURCE_DIR = config.SOURCE_DIR
 const DEV_DIR = config.DEV_DIR
 const DEST_DIR = config.DEST_DIR
-const SERVER_APP_DIR = config.SERVER_APP_DIR
 const CSS = config.CSS
 const JS = config.JS
 const IMAGES = config.IMAGES
@@ -15,7 +13,7 @@ const PDFS = config.PDFS
 const VIDEOS = config.VIDEOS
 
 const REVABLE = [].concat(JS, CSS, IMAGES, FONTS, PDFS, VIDEOS).map(
-  asset => path.join(DEV_DIR, asset)
+  (asset) => path.join(DEV_DIR, asset)
 )
 
 module.exports = () => {
